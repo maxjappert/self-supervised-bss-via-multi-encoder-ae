18879c119e8ae1d338b1afcbb9966dedb6c39687
Max Jappert <max.jappert@gmail.com>
2024-06-04 17:04:12 +0100

    Continued trying to get it to work

--- hp_opti.py
+++ hp_opti.py
@@ -7,15 +7,12 @@ def objective(trial):
     channel_options = [[2, 4, 8], [4, 8, 16], [8, 16, 32], [16, 32, 64], [32, 64, 128], [64, 128, 256], [24, 48, 96, 144], [16, 32, 64, 128]]
 
     # Suggest hyperparameters
-    lr = trial.suggest_loguniform('lr', 1e-5, 1e-1)
-    weight_decay = trial.suggest_loguniform('weight_decay', 1e-10, 1.0)
-    z_decay = trial.suggest_loguniform('z_decay', 1e-10, 1.0)
-    sep_lr = trial.suggest_loguniform('sep_lr', 1e-10, 1.0)
-    zero_lr = trial.suggest_loguniform('zero_lr', 1e-10, 1.0)
-    hidden = trial.suggest_int('hidden', 16, 4096, step=16)
+    sep_lr = trial.suggest_float('sep_lr', 0.0, 1.0, step=0.1)
+    zero_lr = trial.suggest_float('zero_lr', 0.0, 0.5, step=0.01)
+    hidden = trial.suggest_int('hidden', 64, 1024, step=64)
     channel_index = trial.suggest_int('channel_index', 0, len(channel_options)-1, step=1)
-    sep_norm = trial.suggest_categorical('sep_norm', ['L1', 'L2'])
     norm_type = trial.suggest_categorical('norm_type', ['none', 'batch_norm', 'group_norm'])
+    weight_decay = trial.suggest_float('weight_decay', 0.0, 0.0001, step=0.000001)
 
     channels = channel_options[channel_index]
 
@@ -28,16 +25,14 @@ def objective(trial):
         dataset_trainval=dataset_trainval,
         dataset_split_ratio=0.8,
         batch_size=512,
-        sep_norm=sep_norm,
+        sep_norm='L1',
         sep_lr=sep_lr,
         zero_lr=zero_lr,
-        lr=lr,
+        lr=0.001,
         lr_step_size=50,
-        lr_gamma=0.1,
+        lr_gamma=1.0,
         weight_decay=weight_decay,
-        z_decay=z_decay,
-        max_epochs=50,
-
+        max_epochs=60,
         verbose=False
     )
 
@@ -49,7 +44,7 @@ def objective(trial):
 
 # Set up the Optuna study
 study = optuna.create_study(direction='minimize')
-study.optimize(objective, n_trials=20)
+study.optimize(objective, n_trials=200)
 
 # Print the best hyperparameters
 print("Best hyperparameters: ", study.best_params)
--- script.py
+++ script.py
@@ -2,11 +2,19 @@ from functions import *
 
 #print(len(CircleTriangleDataset()))
 
-hp = {'lr': 0.00016062499813462855, 'weight_decay': 0.00039653179743469276, 'z_decay': 1.0277939180373177e-10, 'sep_lr': 0.0013962320530318257, 'zero_lr': 9.929284549910692e-05, 'hidden': 1696, 'channel_index': 5, 'sep_norm': 'L1', 'norm_type': 'none'}
+#hp = {'lr': 0.00016062499813462855, 'weight_decay': 0.00039653179743469276, 'z_decay': 1.0277939180373177e-10, 'sep_lr': 0.0013962320530318257, 'zero_lr': 9.929284549910692e-05, 'hidden': 1696, 'channel_index': 5, 'sep_norm': 'L1', 'norm_type': 'none'}
 
-channel_options = [[2, 4, 8], [4, 8, 16], [8, 16, 32], [16, 32, 64], [32, 64, 128], [64, 128, 256], [24, 48, 96, 144], [16, 32, 64, 128]]
+#channel_options = [[2, 4, 8], [4, 8, 16], [8, 16, 32], [16, 32, 64], [32, 64, 128], [64, 128, 256], [24, 48, 96, 144], [16, 32, 64, 128]]
 
-model, _, _ = train(CircleTriangleDataset(), channels=[24, 48, 96, 144], hidden=96, norm_type='none', batch_size=1024, sep_norm='L1', lr=0.001, weight_decay=1e-05, sep_lr=0.5, zero_lr=0.01, z_decay=0.01, max_epochs=100, name='previously_best', visualise=False, linear=False)
+model, _, _ = train(CircleTriangleDataset(), channels=[16, 32, 64, 128, 256], hidden=96, norm_type='none', batch_size=256, sep_norm='L2', lr=0.001, weight_decay=1e-05, sep_lr=0.8, zero_lr=0.1, z_decay=0.01, max_epochs=100, name='new_attempt', visualise=False, linear=False)
+
+print(test(model, CircleTriangleDataset(), visualise=True, num_samples=1, name='test_alt_1'))
+print(test(model, CircleTriangleDataset(), visualise=True, num_samples=1, name='test_alt_2'))
+print(test(model, CircleTriangleDataset(), visualise=True, num_samples=1, name='test_alt_3'))
+print(test(model, CircleTriangleDataset(), visualise=True, num_samples=1, name='test_alt_4'))
+print(test(model, CircleTriangleDataset(), visualise=True, num_samples=1, name='test_alt_5'))
+
+model, _, _ = train(CircleTriangleDataset(), channels=[24, 48, 96, 144], hidden=96, norm_type='group_norm', batch_size=1024, sep_norm='L1', lr=0.001, weight_decay=1e-05, sep_lr=0.5, zero_lr=0.01, z_decay=0.01, max_epochs=100, name='previously_best', visualise=False, linear=False)
 #model, _, _ = train(CircleTriangleDataset(), channels=[24, 48, 96, 144], hidden=96, norm_type='none', batch_size=1024, sep_norm='L1', lr=0.001, weight_decay=0, sep_lr=1, zero_lr=0.1, z_decay=0, max_epochs=100, visualise=False, linear=False)
 #model, _, _ = train(CircleTriangleDataset(), channels=[24, 48, 96, 144], hidden=128, norm_type='none', batch_size=1024, sep_norm='L2', lr=0.0001, weight_decay=1e-05, sep_lr=0.5, zero_lr=0.01, z_decay=0.01, max_epochs=50, visualise=False, linear=True)
 #model, _, _ = train(CircleTriangleDataset(), channels=channel_options[hp['channel_index']], hidden=hp['hidden'], norm_type=hp['norm_type'], batch_size=512, sep_norm=hp['sep_norm'], lr=hp['lr'], weight_decay=hp['weight_decay'], sep_lr=hp['sep_lr'], zero_lr=hp['zero_lr'], z_decay=hp['z_decay'], max_epochs=100, visualise=False)
@@ -20,7 +28,7 @@ print(test(model, CircleTriangleDataset(), visualise=True, num_samples=1, name='
 print(test(model, CircleTriangleDataset(), visualise=True, num_samples=1, name='test4'))
 print(test(model, CircleTriangleDataset(), visualise=True, num_samples=1, name='test5'))
 
-model, _, _ = train(CircleTriangleDataset(), channels=[24, 48, 96, 144], hidden=128, norm_type='none', batch_size=1024, sep_norm='L2', lr=0.0001, weight_decay=1e-05, sep_lr=0.5, zero_lr=0.01, z_decay=0.01, max_epochs=100, visualise=False, linear=True)
+model, _, _ = train(CircleTriangleDataset(), channels=[16, 32, 64, 128], hidden=128, norm_type='none', batch_size=1024, sep_norm='L2', lr=0.0001, weight_decay=1e-05, sep_lr=0.5, zero_lr=0.01, z_decay=0.01, max_epochs=100, visualise=False, linear=True)
 
 visualise_linear(model, CircleTriangleDataset(), visualise=True, num_samples=1, name='test_linear1')
 visualise_linear(model, CircleTriangleDataset(), visualise=True, num_samples=1, name='test_linear2')
